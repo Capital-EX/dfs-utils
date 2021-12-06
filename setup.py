3eb511a9e86453cd3f5e4591ab80cfe48fe58ac3
Capital-EX <capitalex@protonmail.com>
2021-12-05 21:20:32 -0500

    Change URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	version="0.1.0",
 	description="Utilities for working with LEGO Universe file formats and network packets.",
 	author="lcdr",
-	url="https://github.com/lcdr/utils/",
+	url="https://github.com/Capital-EX/dfs-utils/",
 	license="AGPL v3",
 	packages=["utils"],
 	python_requires=">=3.6",
